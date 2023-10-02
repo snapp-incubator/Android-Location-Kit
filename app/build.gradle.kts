b2845b693cf3b539c3424d87cf68ff281769bf67
Ali Moghadam <ali.moghadam@snapp.cab>
2023-10-02 23:13:43 +0330

    Implement demo app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
+    implementation(project(mapOf("path" to ":location-kit")))
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(project(mapOf("path" to ":location-kit")))
+    implementation("com.karumi:dexter:6.2.3")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/cab/snapp/location/MainActivity.kt
+++ app/src/main/java/cab/snapp/location/MainActivity.kt
@@ -1,11 +1,122 @@
 package cab.snapp.location
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.location.Location
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import cab.snapp.location.databinding.ActivityMainBinding
+import cab.snapp.locationkit.contract.SnappLocationProvider
+import cab.snapp.locationkit.helper.ResolvableApiExceptionHelper
+import cab.snapp.locationkit.implementation.LocationServiceBuilder
+import cab.snapp.locationkit.model.NullLocation
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
+    private lateinit var locationPermissionHelper: LocationPermissionHelper
+    private var locationSub: Disposable? = null
+    private var locationMockSub: Disposable? = null
+    private lateinit var snappLocationProvider: SnappLocationProvider
+    private var lastLocation: Location? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        locationPermissionHelper = LocationPermissionHelper(this)
+
+        snappLocationProvider = LocationServiceBuilder(this)
+            .withUpdateInterval(10000L)
+            .withFastestUpdateInterval(10000L / 2)
+            .mockDistanceTrashHoldInMeter(1000)
+            .mockRealLocationTrashHold(20)
+            .freshLocationUpdateCount(2)
+            .forceUsingDeviceLocationSystemOnly(false)
+            .build()
+
+        locationSub = snappLocationProvider.getLocationStream()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                if (it is NullLocation) {
+                    it.exception?.let { exception ->
+                        ResolvableApiExceptionHelper.startResolutionForResult(
+                            this, exception, 1001
+                        )
+                    } ?: run {
+                        Toast.makeText(this, "null location", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    lastLocation = it
+                    showLocationDetails(it)
+                }
+
+            }
+
+        locationMockSub = snappLocationProvider.getMockLocationStream()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                binding.locationDetails.locationStatus.text =
+                    if (it) getString(R.string.mock_location) else getString(R.string.real_location)
+            }
+
+        binding.buttonStart.setOnClickListener {
+            startGettingLocation()
+        }
+
+        binding.buttonStop.setOnClickListener {
+            stopGettingLocation()
+        }
+
+        binding.buttonGetLocation.setOnClickListener {
+            getLocationOneTime()
+        }
+    }
+
+    private fun showLocationDetails(location: Location) {
+        binding.locationDetails.accuracy.text = location.accuracy.toString()
+        binding.locationDetails.latitude.text = location.latitude.toString()
+        binding.locationDetails.longitude.text = location.longitude.toString()
+    }
+
+    override fun onDestroy() {
+        snappLocationProvider.stopLocationUpdates()
+        locationSub?.takeUnless { it.isDisposed }?.dispose()
+        locationMockSub?.takeUnless { it.isDisposed }?.dispose()
+        _binding = null
+        super.onDestroy()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun startGettingLocation() {
+        locationPermissionHelper.getLocationPermission {
+            binding.locationDetails.serviceStatus.text = getString(R.string.started)
+            snappLocationProvider.startLocationUpdates()
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun stopGettingLocation() {
+        binding.locationDetails.serviceStatus.text = getString(R.string.stopped)
+        snappLocationProvider.stopLocationUpdates()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getLocationOneTime() {
+        locationPermissionHelper.getLocationPermission {
+            snappLocationProvider.getLocation { providedLocation ->
+                providedLocation?.let {
+                    showLocationDetails(it)
+                }
+            }
+        }
     }
 }
