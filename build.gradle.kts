7576b999d7766ed13f90328efd572f5006942021
Ali Moghadam <ali.moghadam@snapp.cab>
2023-10-03 23:35:46 +0330

    Add a publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,6 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.1.1" apply false
+    id("com.vanniktech.maven.publish") version "0.25.3" apply false
+    id("org.jetbrains.dokka") version "1.8.20" apply false
 }
